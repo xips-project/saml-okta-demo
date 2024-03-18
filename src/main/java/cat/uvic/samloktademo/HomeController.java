d8a7fd80dc5aeb44405d65845009111ed3eec833
Alex <7701272+afcasco@users.noreply.github.com>
2024-03-18 19:54:43 +0100

    add request jwt from api button and logic

--- src/main/java/cat/uvic/samloktademo/HomeController.java
+++ src/main/java/cat/uvic/samloktademo/HomeController.java
@@ -1,10 +1,19 @@
 package cat.uvic.samloktademo;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.saml2.provider.service.authentication.Saml2AuthenticatedPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+
 
 @Controller
 public class HomeController {
@@ -14,6 +23,26 @@ public class HomeController {
         model.addAttribute("name", principal.getName());
         model.addAttribute("emailAddress", principal.getFirstAttribute("email"));
         model.addAttribute("userAttributes", principal.getAttributes());
+        return "home";
+    }
+
+    @RequestMapping("/getJwt")
+    public String getJwt(@AuthenticationPrincipal Saml2AuthenticatedPrincipal principal, Model model){
+        RestTemplate restTemplate = new RestTemplate();
+
+        String url = "http://localhost:8081/auth/login";
+        String requestJson = "{\"username\":\"test\",\"password\":\"test\"}";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> entity = new HttpEntity<>(requestJson, headers);
+        HttpEntity<String> answer = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
+        HttpHeaders responseHeaders = answer.getHeaders();
+        String cookie = responseHeaders.getFirst(HttpHeaders.SET_COOKIE);
+        System.out.println(cookie);
+
+
+        model.addAttribute("jwtToken",cookie);
+
         return "home";
     }
 }
