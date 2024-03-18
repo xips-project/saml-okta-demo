0406f18a88855a96223b7fd7874543b9ee36da68
Alex <7701272+afcasco@users.noreply.github.com>
2024-03-18 12:59:05 +0100

    initial commit

---
+++ src/main/java/cat/uvic/samloktademo/SamlOktaDemoApplication.java
@@ -0,0 +1,13 @@
+package cat.uvic.samloktademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamlOktaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SamlOktaDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cat/uvic/samloktademo/SamlOktaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.uvic.samloktademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamlOktaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
